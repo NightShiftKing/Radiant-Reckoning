64105b460b6adf4caf5c8c22478c5002ad4a93e7
ThatGoblinKing <gusjminecritter@gmail.com>
2024-09-17 23:41:01 -0600

    Fixed Keyboard Input, Added launch.json and gitignore

--- CharacterObject.py
+++ CharacterObject.py
@@ -5,31 +5,15 @@ class Character(GameObject):
     def __init__(self, xpos, ypos, name, description, health, damage, defense, level=1, experience=0):
         super().__init__(xpos, ypos, name, description, "character", health, damage, defense)
         self.level = level
-        self.experience = experience 
+        self.experience = experience
+        self.pos = pygame.Vector2(xpos, ypos)
+        self.velocity = pygame.Vector2(0, 0)
 
-    def move(self, gameEvents, speed = 10):
-
+    def move(self, speed = 10):
         keys = pygame.key.get_pressed()
-        for event in gameEvents:  # quit game if x is pressed in top corner
-            if event.type == pygame.KEYDOWN:  # keyboard input
-                if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-                    self.xpos -= speed
-                elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-                    self.xpos += speed
-                elif keys[pygame.K_RIGHT] or keys[pygame.K_w]:
-                    self.ypos -= speed
-                elif keys[pygame.K_RIGHT] or keys[pygame.K_s]:
-                    self.ypos += speed
-
-
-
-
-
-
-
-
-
-
+        self.velocity.x = speed if keys[pygame.K_RIGHT] or keys[pygame.K_d] else -speed if keys[pygame.K_LEFT] or keys[pygame.K_a] else 0
+        self.velocity.y = speed if keys[pygame.K_DOWN] or keys[pygame.K_s] else -speed if keys[pygame.K_UP] or keys[pygame.K_w] else 0
+        self.pos += self.velocity
 
     def draw(self, screen):
-        pygame.draw.rect(screen, (255, 0, 255), (self.xpos, self.ypos, 20, 20)) 
+        pygame.draw.rect(screen, (255, 0, 255), (self.pos.x, self.pos.y, 20, 20)) 
